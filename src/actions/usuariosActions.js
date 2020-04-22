f4337353c64636e7d8a3bd9df5d5150ef817e869
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-04-21 22:08:41 -0500

    Implementado mejores practicas

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,13 +1,25 @@
-import { TRAER_TODOS } from '../types/usuariosTypes';
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/usuariosTypes';
 import axios from 'axios';
 
 export const traerTodos = () => async (dispatch) => {
-  const respuesta = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
-  );
-
   dispatch({
-    type: TRAER_TODOS,
-    payload: respuesta.data,
+    type: CARGANDO,
   });
+
+  try {
+    const respuesta = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+
+    dispatch({
+      type: TRAER_TODOS,
+      payload: respuesta.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: 'Ups, algo salió mal, intento de nuevo luego',
+    });
+    console.log('Error', error.message);
+  }
 };
